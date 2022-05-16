3cedf7a0b4b9e9ff7af04972db2cc8d5df84de9e
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-05-16 16:24:58 -0700

    hof challenges

---
+++ hof-jb-lr.js
@@ -0,0 +1,47 @@
+// Write a function that takes in an array of numbers and returns a new array with all numbers multiplied by 10.
+var arr1 = [3, 9, 15, 4, 10]
+const moulTen = (array) => {
+    let multiply = array.map(value => {
+        return value *10
+    }) 
+    console.log(multiply)
+}
+moulTen(arr1)
+// // --> [30, 90, 150, 40, 100]
+
+
+// Write a function that takes in an array of numbers and returns a new array with only odd numbers.
+var arr2 = [2, 7, 3, 5, 8, 10, 13]
+// // --> [7, 3, 5, 13]
+const newArray = (array) => {
+    let oddArr = array.filter(value => {
+        return value % 2 !== 0
+    })
+    console.log(oddArr)
+}
+newArray(arr2)
+// Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: Use typeof method
+var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
+// // --> "nicework"
+const onlyLetters = (array) => {
+     let type = array.filter(value => {
+         return typeof value === "string"
+     })
+console.log(type.join(""))
+    }
+    onlyLetters(comboArr)
+
+
+
+// Create a function that takes in an array and returns an array without any false, null, 0, or blank values.
+// var filterArrayValues = [58, " ", "abcd", true, null, false, 0]
+// // --> [58, "abcd", true]
+// STRETCH Challenges
+
+// Create a function that takes in a string and returns a new string with all the vowels removed.
+// var str = "javascript is awesome"
+// // --> "jvscrpt s wsm"
+// Create a function that takes in two arrays as arguments returns one array with no duplicate values.
+// var arr1 = [3, 7, 10, 5, 4, 3]
+// var arr2 = [7, 8, 2, 1, 5, 4]
+// // --> [3, 7, 10, 5, 4, 8, 2, 1]
