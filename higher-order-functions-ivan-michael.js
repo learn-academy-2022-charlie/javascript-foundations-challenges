9b29a9fe0ddd42c2ed516f5ac5b7f7b8f02e966e
michael-a-tobias <michael.a.tobias@outlook.com>
2022-05-16 16:40:38 -0700

    We are on the last question.

--- higher-order-functions-ivan-michael.js
+++ higher-order-functions-ivan-michael.js
@@ -15,21 +15,56 @@ const timesTen = arr1.map((value)=>{
 
 
 // Write a function that takes in an array of numbers and returns a new array with only odd numbers. 
+var arr2 = [2, 7, 3, 5, 8, 10, 13]
 
+// I will create a function that will take an array, look at each value, and .filter() each one, returning only the odd numbers.
 
+const oddsOnly = arr2.filter((value) => {
+	// create a constant that equal to the number array filtered where each iteration looks at the element's value
+	if (value % 2 !== 0) {
+		// if the value divided by 2 does not have a remainder of 0, it is odd
+		return value
+		// if it meets the above parameters, return it
+	}
+})
 
+console.log(oddsOnly)
 
+// // Expected Output: --> [7, 3, 5, 13]
+// // Output: [7, 3, 5, 13]
 
-
-
-// var arr2 = [2, 7, 3, 5, 8, 10, 13]
-// // --> [7, 3, 5, 13]
 // Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: Use typeof method
-// var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
-// // --> "nicework"
+var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
+
+// I will create a function that will take an array with any data inside it. It will then .filter() through each one and return the element if the typeof === "string" is true.
+
+const lettersOnly = comboArr.filter((value) => {
+	// the constant lettersOnly = an array with a filter that looks at element values
+	if (typeof value === "string") {
+		// if the value type is a string...
+		return value
+		//... return the value
+	}
+})
+console.log(lettersOnly.join(""))
+
+// the normal return is an array of individual characters, .join("") brings them together into one string
+
+// // Expected Output: --> "nicework"
+// Output: "nice work"
+
 // Create a function that takes in an array and returns an array without any false, null, 0, or blank values.
-// var filterArrayValues = [58, " ", "abcd", true, null, false, 0]
+var filterArrayValues = [58, " ", "abcd", true, null, false, 0]
+
+//  I will create a funciton that takes an array that has a filter. The filter will look at the value of each element. I will use a conditional statement to select values that are NOT false, null, 0, or blank.
+
 // // --> [58, "abcd", true]
+
+
+
+
+
+
 // STRETCH Challenges
 
 // Create a function that takes in a string and returns a new string with all the vowels removed.
