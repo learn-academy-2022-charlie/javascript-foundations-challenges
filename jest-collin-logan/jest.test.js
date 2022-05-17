116d5af1b812366af88ffd1b8ef8a6265beee634
Renzlawr <RSLogan23@gmail.com>
2022-05-17 16:12:10 -0600

    passed tests 1 and 2

---
+++ jest-collin-logan/jest.test.js
@@ -0,0 +1,107 @@
+// const { describe } = require("yargs")
+
+// Write the test for a function that returns "drink coffee" if you are tired and "keep working" if you are not tired.
+describe("alertness", () => {
+    it("returns drink coffee if tired or keep working if not tired", () => {
+        expect(alertness("tired")).toEqual("drink coffee")
+        expect(alertness("not tired")).toEqual("keep working")
+    })
+})
+
+//  FAIL  ./jest.test.js
+//   alertness
+//   ✕ returns drink coffee if tired or keep working if not tired (3 ms)
+
+// ● alertness › returns drink coffee if tired or keep working if not tired
+
+//   ReferenceError: alertness is not defined
+
+// Write the function that will make the test pass.
+
+const alertness = (prompt) => {
+    if(prompt === "tired") {
+        return "drink coffee"
+    } else if (prompt === "not tired") {
+        return "keep working"
+    } 
+}
+
+alertness()
+
+// PASS  ./jest.test.js
+// alertness
+//   ✓ returns drink coffee if tired or keep working if not tired (8 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+// Snapshots:   0 total
+// Time:        1.547 s, estimated 2 s
+// Ran all test suites.
+// ✨  Done in 5.95s.
+
+// Write the test for a function that returns "relax" if you are stressed and "keep going" if you are not stressed.
+
+
+describe("stressLevel", () => {
+    it("returns relax if you are stressed and keep going if you are not stressed", () => {
+        expect(stressLevel("stressed")).toEqual("relax")
+        expect(stressLevel("relaxed")).toEqual("keep going")
+    })
+})
+
+
+// FAIL  ./jest.test.js
+//   ● Test suite failed to run
+
+//     Your test suite must contain at least one test.
+
+//       at onResult (node_modules/@jest/core/build/TestScheduler.js:172:18)
+//           at Array.map (<anonymous>)
+
+// Write the function that will make the test pass.
+
+const stressLevel = (string) => {
+    if(string === "stressed") {
+        return "relax"
+    } else if (string === "relaxed") {
+        return "keep going"
+    }
+}
+
+stressLevel()
+
+// PASS  ./jest.test.js
+// alertness
+//   ✓ returns drink coffee if tired or keep working if not tired (8 ms)
+// stressLevel
+//   ✓ returns relax if you are stressed and keep going if you are not stressed (1 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       2 passed, 2 total
+// Snapshots:   0 total
+// Time:        1.611 s, estimated 2 s
+// Ran all test suites.
+// ✨  Done in 5.58s.
+
+// Write the test for a function that returns "in budget" if a price is lower than $300.
+// Write the function that will make the test pass.
+// Write the test for a function that takes in two arguments(numbers) and returns the smaller number.
+// Write the function that will make the test pass.
+// Write the test for a function that takes in one argument(number) and returns whether the number is odd.
+// Write the function that will make the test pass.
+// Write the test for a function that takes in a fruit and returns "yellow" if the argument is banana, "red" if apple and "purple" if grape.
+// Write the function that will make the test pass.
+// Write the test for a function called rick that returns "Morty".
+// Write the function that will make the test pass.
+// Write the test for a function called greeter that takes a name as an argument and returns a greeting with that name to the screen.
+// Write the function that will make the test pass.
+// Write the test for a function called oddOrEven that takes a number as an argument and logs whether the number is odd or even.
+// Write the function that will make the test pass.
+// Write the test for a function called doubler that takes a number and returns the result of the number multiplied by 2.
+// Write the function that will make the test pass.
+// Write the test for a function called multiply that takes two numbers as arguments and logs the result of one of the numbers multiplied by the other.
+// Write the function that will make the test pass.
+// Write the test for a function called divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+// Write the function that will make the test pass.
+// Write the test for a function called fizzbuzz. If a number is a multiple of 3, replace it with the word 'fizz'. If a number is a multiple of five, replace it with the word 'buzz'. If a number is a multiple of both 3 and 5, replace it with 'fizzbuzz'.
+// Write the function that will make the test pass.
