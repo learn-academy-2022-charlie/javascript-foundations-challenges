8091971d4f765420e5dd5b9bcba59a4612f54199
LEARN Academy <learnacademy@LEARNs-MacBook-Air-3.local>
2022-05-18 15:33:08 -0700

    completed first 4 questions

---
+++ classes-davon-stephen.js
@@ -0,0 +1,64 @@
+// Coffee Maker
+class Coffee {
+  constructor(type, cream, sugar){
+    this.type = type.toLowerCase()
+    this.cream = cream
+    this.sugar = sugar
+  }
+
+  coffeeProfile(){
+    return `A ${this.type} coffee with ${this.creams()}, ${this.sugars()}`
+  }
+
+  creams(){
+    if(this.cream > 1){
+      return `${this.cream} creams`
+    } else {
+      return `${this.cream} cream`
+    }
+  }
+
+  sugars(){
+    if(this.sugar > 1){
+      return `${this.sugar} sugars`
+    } else {
+      return `${this.sugar} sugar`
+    }
+  }
+}
+// Write the code that makes a black coffee. Then write the code that outputs the coffee's profile.
+var blackCoffee = new Coffee("black", 0, 0)
+console.log(blackCoffee.coffeeProfile());
+
+//output: ---->A black coffee with 0 cream, 0 sugar
+
+// Write the code that makes a coffee with 1 cream and 2 sugars. Then write the code that outputs the coffee's profile.
+var icedCoffee = new Coffee("iced", 1, 2)
+console.log(icedCoffee.coffeeProfile())
+//output: ---->A iced coffee with 1 cream, 2 sugars
+
+// Latte Maker
+// Write a Latte class that takes a flavor, a milk type and a number of shots.
+class Latte {
+  constructor(flavorTypeString, milkTypeString, espressoShotsNumb){
+    this.flavor = flavorTypeString
+    this.milk = milkTypeString
+    this.espresso = espressoShotsNumb
+  }
+  latteProfile(){
+    return `A ${this.flavor} latte with ${this.milk} and ${this.espresso} espresso shots`
+  }
+}
+// Write a method for your Latte class that outputs the latte's profile.
+// Write the code that makes a regular, single shot latte. Then, log the latte's profile.
+var regularLatte = new Latte("caramel", "oat milk", 1)
+console.log(regularLatte.latteProfile());
+//output: ---->A caramel latte with oat milk and 1 espresso shots
+
+// Write the code that makes a double shot hazelnut latte with almond milk. Then, log the latte's profile.
+var doubleLatte = new Latte("Hazelnut", "almond milk", 2)
+console.log(doubleLatte.latteProfile());
+//output: ---->A Hazelnut latte with almond milk and 2 espresso shots
+
+// Volume of a Cylinder
+// Write a class that calculates the volume of a Cylinder to four decimal places. Volume of a cylinder : V = πr2h (r is the radius and h is the height of the cylinder)
