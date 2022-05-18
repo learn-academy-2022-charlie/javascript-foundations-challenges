004fd8e4919528c2dea83e9b0f739656555026d2
Don Yoon <dongho.yoon@gmail.com>
2022-05-18 15:13:49 -0700

    object destructuring is destructuring my brain

--- objects-lance-don.js
+++ objects-lance-don.js
@@ -68,58 +68,126 @@ lunch.sandwich = function () {
 console.log(lunch.sandwich())
 // Consider this variable:
 
-// const animals = [
-//   { name: "Waffles", type: "dog", age: 7 },
-//   { name: "Fluffy", type: "cat", age: 14 },
-//   { name: "Spelunky", type: "dog", age: 4 },
-//   { name: "Hank", type: "cat", age: 11 }
-// ]
+const animals = [
+  { name: "Waffles", type: "dog", age: 7 },
+  { name: "Fluffy", type: "cat", age: 14 },
+  { name: "Spelunky", type: "dog", age: 4 },
+  { name: "Hank", type: "cat", age: 11 }
+]
 // Create a function that takes in an array of animal objects and returns a new array with only the objects that are cats.
+
+const onlyCats = (array) => {
+  return animals.filter(value => value.type === "cat")
+}
+
+console.log(onlyCats(animals))
 // Create a function that takes in an array of animal objects and returns a new array with only the names of the animals.
+
+const onlyNames = (array) => {
+  return animals.map(value => value.name)
+}
+
+console.log(onlyNames(animals))
 // Create a function that takes in an array of animal objects and returns a new array of the names of the animals that are more than 10 years old.
+
+const namesAgeTen = (array) => {
+  let ageTen = animals.filter(value => value.age >10) 
+    return ageTen.map(value => value.name)
+  
+}
+console.log(namesAgeTen(animals))
 // Create a function that takes in an array of animal objects and returns a new array with a sentence about each animal.
+const describeAnimals = (array) => {
+    return array.map(value => `${value.name} is a ${value.age} old ${value.type}`)
+}
+console.log(describeAnimals(animals))
+
 // Consider this variable:
 
-// const author = {
-//   name: "H. G. Wells",
-//   genre: "science fiction"
-// }
+const author = {
+  name: "H. G. Wells",
+  genre: "science fiction"
+}
+
 // Write the code that destructures the author object so that the following code snippet will run successfully:
-// console.log(`${name} is a ${genre} author`)
-// // Output: "H. G. Wells is a science fiction author"
+
+const {name, genre} = author
+
+console.log(`${name} is a ${genre} author`)
+// Output: "H. G. Wells is a science fiction author"
 // Consider this variable:
 
-// const pokeOne = {
-//   species: "Charmandar",
-//   pokemon_type: "Fire"
-// }
+const pokeOne = {
+  species: "Charmandar",
+  pokemon_type: "Fire"
+}
 
-// const pokeTwo = {
-//   species: "Magikarp",
-//   pokemon_type: "Water"
-// }
+const pokeTwo = {
+  species: "Magikarp",
+  pokemon_type: "Water"
+}
+
 // Create a function called describePokemon that take an object like the ones above and uses destructuring to return a description of the Pokemon so that the following code snippet will run successfully:
-// console.log(describePokemon(pokeOne))
-// // Output: "Charmandar is a Fire pokemon"
-// console.log(describePokemon(pokeTwo))
-// // Output: "Magikarp is a Water pokemon"
+const describePokemon = (object) => {
+  let {species, pokemon_type} = object
+  return `${species} is a ${pokemon_type} pokemon`
+}
+
+console.log(describePokemon(pokeOne))
+// Output: "Charmandar is a Fire pokemon"
+console.log(describePokemon(pokeTwo))
+// Output: "Magikarp is a Water pokemon"
+
 // Consider this variable:
 
-// const triangleDimensions = {
-//   base: 2,
-//   height: 5
-// }
+const triangleDimensions = {
+  base: 2,
+  height: 5
+}
+
 // Modify the triangleDimensions object to have a method that returns the area of the triangle.
+triangleDimensions.area = function () {
+  return this.base * this.height * 0.5
+}
+console.log(triangleDimensions.area())
 // Write the code that will update the base to be the value of 6.
+triangleDimensions.base = 6
+console.log(triangleDimensions)
+
+
 // Consider this variable:
 
-// const learn = {
-//   cohorts: {
-//     "2021": ["Alpha", "Bravo", "Charlie", "Delta", "Echo"],
-//     "2022": ["Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot"]
-//   }
-// }
+const learn = {
+  cohorts: {
+    "2021": ["Alpha", "Bravo", "Charlie", "Delta", "Echo"],
+    "2022": ["Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot"]
+  }
+}
 // Write the code that logs the name of your cohort.
+// console.log(`${learn["cohorts"]["2022"]} ${learn["cohorts"]["2022"][2]}`)
+console.log(`${Object.keys(learn.cohorts)[1]} ${learn.cohorts["2022"][2]}`)
 // Write the code that uses destructuring to log the name of your cohort.
+
+
+const { 
+        cohorts: {
+          ["2021"]: array1, 
+          ["2022"]: array2
+        }
+      } = learn
+
 // Create a function that takes an object like the one above and returns an array with a string of every cohort name and year.
+
+// const yearCohort = (object, yearStr, cohortStr) => {
+//   //TODO find some object methods to extract keys, use array to find length to see how we need to destructure the object
+  
+//   let nestOne = Object.keys(object)[Object.keys(object).length - 1]
+//   console.log(nestOne)
+//   let nestTwo = Object.keys(object[nestOne])
+//   console.log(nestTwo)
+//   return `${nestTwo[nestTwo.indexOf(yearStr)]} ${object[nestOne][yearStr].indexOf(cohortStr)}`
+// }
+
+// console.log(yearCohort(learn, "2022", "Charlie"))
 // // Output: ["2021 Alpha", "2021 Bravo", "2021 Charlie", "2021 Delta", "2021 Echo", "2022 Alpha", "2022 Bravo", "2022 Charlie", "
+
