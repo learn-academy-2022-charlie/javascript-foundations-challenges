dbe7b84e0faa998a57cf4e86e9a7b575b7810a60
Renzlawr <RSLogan23@gmail.com>
2022-05-18 13:52:39 -0600

    completed some challenges

--- objects-collin-logan.js
+++ objects-collin-logan.js
@@ -50,10 +50,18 @@ const product = {
 const describeProduct = (value) => {
     return `The product is a ${value.name}. It cost ${value.price}`
 }
-console.log(describeProduct(product))
+// console.log(describeProduct(product))
 
 
 // Write a function called totalWithTax that takes the product object as an argument and returns the total price of the chair that includes a 7% sales tax rounded to two decimals.
+
+const totalWithTax = (value) => {
+  let priceWithTax = value.price * .07 + value.price
+  return priceWithTax.toFixed(2)
+}
+
+// console.log(totalWithTax(product))
+
 // Consider this variable:
 
 // const lunch = {
@@ -61,22 +69,88 @@ console.log(describeProduct(product))
 //   type: "sandwich",
 //   ingredients: ["bread", "peanut butter", "banana"]
 // }
+
 // Write the code that accesses the ingredients property.
+
+// console.log(lunch.ingredients)
+
 // Write the code that access the third ingredient of the lunch object.
+
+// console.log(lunch.ingredients[2])
+
 // Write a function that takes the lunch object as an argument and returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+
+const PBB = (lunch) => {
+  return `The ingredients for a ${lunch.name} ${lunch.type} are ${lunch.ingredients.join(" ")}`
+} 
+
+// console.log(PBB(lunch))
+
 // Update the lunch object with method that returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+
+const lunch = {
+  name: "PB and Banana",
+  type: "sandwich",
+  ingredients: ["bread", "peanut butter", "banana"],
+  PBB: function() {
+    return `The ingredients for a ${this.name} ${this.type} are ${this.ingredients.join(" ")}`
+  } 
+}
+
+// console.log(lunch.PBB())
+
 // Consider this variable:
 
-// const animals = [
-//   { name: "Waffles", type: "dog", age: 7 },
-//   { name: "Fluffy", type: "cat", age: 14 },
-//   { name: "Spelunky", type: "dog", age: 4 },
-//   { name: "Hank", type: "cat", age: 11 }
-// ]
+const animals = [
+  { name: "Waffles", type: "dog", age: 7 },
+  { name: "Fluffy", type: "cat", age: 14 },
+  { name: "Spelunky", type: "dog", age: 4 },
+  { name: "Hank", type: "cat", age: 11 }
+]
 // Create a function that takes in an array of animal objects and returns a new array with only the objects that are cats.
+
+const onlyCats = (array) => {
+  return array.filter(value => {
+    return value.type === "cat"
+  })
+}
+
+// console.log(onlyCats(animals))
+
 // Create a function that takes in an array of animal objects and returns a new array with only the names of the animals.
+
+const names = (array) => {
+  return array.map(value => {
+    return value.name
+  })
+}
+
+// console.log(names(animals))
+
 // Create a function that takes in an array of animal objects and returns a new array of the names of the animals that are more than 10 years old.
+
+const newAnimal = (array) => {
+  let olderThanTen = array.filter(value => {
+    return value.age > 10
+  })
+  let names = olderThanTen.map(value => {
+    return value.name
+  })
+  return names
+}
+
+// console.log(newAnimal(animals))
+
 // Create a function that takes in an array of animal objects and returns a new array with a sentence about each animal.
+
+const animalDesc = (array) => {
+  return array.map(value => {
+    return `${value.name} is a ${value.type} that is ${value.age} years old`
+  })
+}
+
+console.log(animalDesc(animals))
+
 // Consider this variable:
 
 // const author = {
