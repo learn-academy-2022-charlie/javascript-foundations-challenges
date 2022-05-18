6e1973f97924f4532ce9e3b04c3fb849b0ab7f86
CJOcode <cjoconnor24@gmail.com>
2022-05-18 16:09:17 -0700

    finished

--- classes-collin-logan.js
+++ classes-collin-logan.js
@@ -47,24 +47,56 @@ class Latte {
         this.numberOfShots = numberOfShots
     }
 
+// Write a method for your Latte class that outputs the latte's profile.
+
     latteProfile(){
-        `This is a ${this.flavor} Latte with ${this.milkType} and ${this.numberOfShots} shots`
+        return `This is a ${this.flavor} Latte with ${this.milkType} and ${this.numberOfShots} shots`
     }
 }
 
-// Write a method for your Latte class that outputs the latte's profile.
-
-// latteProfile(){
-//     `This is a ${this.flavor} Latte with ${this.milkType} and ${this.numberOfShots} shots`
-// }
 
 // Write the code that makes a regular, single shot latte. Then, log the latte's profile.
 
+let latte = new Latte("regular", "milk", 1)
+
+// console.log(latte.latteProfile())
+
+// ---> This is a regular Latte with milk and 1 shots
 
 
 // Write the code that makes a double shot hazelnut latte with almond milk. Then, log the latte's profile.
 
+let hazelLatte = new Latte("hazelnut", "almond milk", 2)
+
+// console.log(hazelLatte.latteProfile())
+
+// This is a hazelnut Latte with almond milk and 2 shots
+
 // Volume of a Cylinder
+
 // Write a class that calculates the volume of a Cylinder to four decimal places. Volume of a cylinder : V = πr2h (r is the radius and h is the height of the cylinder)
 
+class VolumeCylinder {
+  constructor(radius, height){
+    this.radius = radius
+    this.height = height
+  }
+calculateVolume(){
+  let volume = this.radius * this.height * 2 * Math.PI
+  return volume.toFixed(4)
+}
+}
+
+let cylinder = new VolumeCylinder(2,2)
+
+console.log(cylinder.calculateVolume())
+
 // Write the code that creates three unique cylinder objects
+
+let cylinder1 = new VolumeCylinder(7, 5)
+let cylinder2 = new VolumeCylinder(4, 9)
+let cylinder3 = new VolumeCylinder(1, 18)
+
+console.log(cylinder1.calculateVolume())
+console.log(cylinder2.calculateVolume())
+console.log(cylinder3.calculateVolume())
